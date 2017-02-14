08dd520d65d16f41c66787f608e8f1bf1ffdc5f8
Kendal Harland <kjharland@google.com>
2017-02-14 08:11:09 -0800

    fixup interface for AnalysisServer

--- lib/analysis_client.dart
+++ lib/analysis_client.dart
@@ -6,16 +6,15 @@ import 'package:analysis_client/src/domains/server_domain.dart';
 /// To learn more about the analysis server api see:
 /// https://htmlpreview.github.io/?https://github.com/dart-lang/sdk/blob/
 /// master/pkg/analysis_server/doc/api.html
-abstract class AnalysisServer {
+class AnalysisServer {
   /// The domain for server requests.
   final ServerRequestDomain server;
 
   /// The domain for analysis requests.
   final AnalysisRequestDomain analysis;
 
-  /// [communicationSink] is the sink for writing commands to the analysis
-  /// server.
-  AnalysisServer(StringSink communicationSink)
-      : analysis = new AnalysisRequestDomain(communicationSink),
-        server = new ServerRequestDomain(communicationSink);
+  /// [cmdSink] is the sink for writing commands to the analysis server.
+  AnalysisServer(StringSink cmdSink)
+      : analysis = new AnalysisRequestDomain(cmdSink),
+        server = new ServerRequestDomain(cmdSink);
 }
